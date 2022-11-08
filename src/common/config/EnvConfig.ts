623d64d4195f5352d18348d2dd5a33852cae06cf
jujucom <jujucom@126.com>
2022-11-08 11:02:18 +0800

    feat: integrate keycloak

--- src/common/config/EnvConfig.ts
+++ src/common/config/EnvConfig.ts
@@ -1,6 +1,7 @@
 import { KeycloakConfig } from 'keycloak-js';
 
-export const KcConfig: KeycloakConfig = {
+export let KcConfig: KeycloakConfig;
+KcConfig = {
   realm: process.env.REACT_APP_KEYCLOAK_REALM ?? '',
   url: process.env.REACT_APP_KEYCLOAK_URL ?? '',
   clientId: process.env.REACT_APP_CLIENT_ID ?? ''
